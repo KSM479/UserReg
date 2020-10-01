aae0f1d6950d14a5771724ee082b52abd09672d7
Kalpan Mehta <kalpan.mehta@gmail.com>
2020-09-30 23:08:40 -0400

    Docker and SQL code

---
+++ initial-setup.sql
@@ -0,0 +1,4 @@
+CREATE TABLE users (firstname VARCHAR(20), lastname VARCHAR(20), email VARCHAR(20) PRIMARY KEY, password CHAR(20), gender CHAR(1), birthday DATE);
+
+INSERT INTO users values ('Kalpan', 'Mehta', 'kalpanmehta@gmail.com', 'Kalpan@1234', 1, '1997-05-06');
+INSERT INTO users values ('Kelvin', 'Kline', 'Kevin@gmail.com', 'Something@1234', 1, '1967-07-10');
